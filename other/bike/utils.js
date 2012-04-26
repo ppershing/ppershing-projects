d46b0c3be5111632726f4c44c30f6ef5c2ba45ff
ppershing <ppershing@f4dd64ed-df62-21b0-5c13-b0845ec274ee>
2012-04-26 18:04:14 +0000

    Added assert to utils.

--- other/bike/utils.js
+++ other/bike/utils.js
@@ -3,4 +3,18 @@ utils = {
     var mult = Math.pow(10, places);
     return Math.round(number * mult) / mult;
   },
+
+  AssertException: function (message) {
+      this.message = message;
+  },
+
+  assert: function(exp, message) {
+      if (!exp) {
+          throw new utils.AssertException(message);
+      }
+  }
 };
+
+utils.AssertException.prototype.toString = function () {
+    return 'AssertException: ' + this.message;
+}
