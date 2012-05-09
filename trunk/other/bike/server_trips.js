88a20b4ef3bda90ba7fa9b5346ad32bb71c4c8d3
ppershing <ppershing@f4dd64ed-df62-21b0-5c13-b0845ec274ee>
2012-05-09 13:08:49 +0000

    Solved problems with elevation, elevationService now buffers requests, new ability to load trips from the server

---
+++ trunk/other/bike/server_trips.js
@@ -0,0 +1,49 @@
+var server_trips = {
+    TRIP_FILE : "trips",
+    rootElement: ELEMENTS.serverTripsList,
+
+    init: function() {
+        var request = new XMLHttpRequest()
+        request.open("GET", this.TRIP_FILE, true)
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    var trips = JSON.parse(request.responseText);
+                    server_trips.refreshOnPage(trips);
+                } else {
+                    server_trips.refreshOnPage([]);
+                }
+            } else {
+                // pass
+            }
+        }
+        request.send(null);
+    },
+
+    refreshOnPage: function(trips) {
+        var rootElement = this.rootElement;
+
+        // Beware, indexes are shifting!
+        while (rootElement.children.length) {
+          rootElement.removeChild(rootElement.children[0]);
+        }
+
+        var len = trips.length;
+        for (var i = 0; i < len; ++i) {
+            // the entire entry
+            var entry = document.createElement("div");
+            var link = document.createElement("a");
+
+            // TODO: sanitize trips[i].file
+            link.href = "#{|load_from|:|" + trips[i].file + "|}";
+            link.textContent = trips[i].desc;
+
+            entry.appendChild(link);
+            rootElement.appendChild(entry);
+        };
+    }
+
+};
+
+server_trips.init();
+
